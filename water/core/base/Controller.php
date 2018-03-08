acbd4d67534151c5adb924f0d285b4f3f64757ce
Thomas Fazanaro <thomasfazanaro@gmail.com>
2018-03-08 20:47:15 -0300

    Ativado "strict_types" nas classes.

--- water/core/base/Controller.php
+++ water/core/base/Controller.php
@@ -1,17 +1,35 @@
-<?php namespace core\base;
+<?php
+
+declare(strict_types=1);
+
+namespace core\base;
 
 abstract class Controller
 {
     private $model;
 
-    use \core\traits\ClassMethods;
-
     public function __construct($model = null)
     {
-        self::validateNumArgs(__FUNCTION__, func_num_args(), 0, 1);
-        self::validateArgType(__FUNCTION__, $model, 1, ['string', 'object', 'null']);
-
         $this->setModel($model);
+    }
+
+    abstract public function index();
+
+    final protected function loadModel(string $name): Model
+    {
+        if (is_string($name)) {
+            $namespace = 'model\\'.str_replace(DS, '\\', $name);
+            $model = new $namespace();
+
+            return $model;
+        }
+
+        return null;
+    }
+
+    final protected function model(): Model
+    {
+        return $this->model;
     }
 
     private function setModel($model)
@@ -19,32 +37,11 @@ abstract class Controller
         $this->model = null;
 
         if (is_string($model)) {
-            $namespace = 'model\\' . str_replace(DS, '\\', $model);
-            $this->model = new $namespace;
+            $namespace = 'model\\'.str_replace(DS, '\\', $model);
+            $this->model = new $namespace();
         }
-        if (is_object($model)) {
+        if ($model instanceof Model) {
             $this->model = $model;
         }
-    }
-
-    abstract function index();
-
-    protected final function loadModel($model)
-    {
-        self::validateNumArgs(__FUNCTION__, func_num_args(), 1, 1);
-        self::validateArgType(__FUNCTION__, $model, 1, ['string']);
-
-        if (is_string($model)) {
-            $namespace = 'model\\' . str_replace(DS, '\\', $model);
-            $model = new $namespace;
-            return $model;
-        }
-        return null;
-    }
-
-    protected final function model()
-    {
-        self::validateNumArgs(__FUNCTION__, func_num_args());
-        return $this->model;
     }
 }
